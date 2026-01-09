b413fb19fa1510793a82802f044a84dfcc1a3560
Lauren Hendley <116697229+laurenhendley@users.noreply.github.com>
2026-01-09 09:55:37 -0500

    Add files via upload

---
+++ commonPref.java
@@ -0,0 +1,14 @@
+class commonPref {
+    public String longestCommonPrefix(String[] strs) {
+        String pref = strs[0];
+
+        for(int i = 0; i < strs.length; i++){
+            while(!strs[i].startsWith(pref)){
+                pref = pref.substring(0, pref.length() - 1);
+                if(pref.isEmpty()) return "";
+            }
+        }
+
+        return pref;
+    }
+}
