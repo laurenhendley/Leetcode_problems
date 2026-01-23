373c3ce12ca72d07f5e3719021d6f3ff998b0165
laurenhendley <lauren.hendley4@gmail.com>
2026-01-23 06:48:32 -0500

    Solved array to tree problem

---
+++ easy/ArrayToTree.go
@@ -0,0 +1,23 @@
+package easy
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+
+	mid := len(nums) / 2
+
+	root := &TreeNode{
+		Val:   nums[mid],
+		Left:  sortedArrayToBST(nums[:mid]),
+		Right: sortedArrayToBST(nums[mid+1:]),
+	}
+
+	return root
+}
