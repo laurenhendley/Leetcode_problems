0d583c976f25cfc6713f145f48ea4b4ead54e0d9
laurenhendley <lauren.hendley4@gmail.com>
2026-01-24 09:23:51 -0500

    Solved min tree depth problem

---
+++ easy/MinTreeDepth.go
@@ -0,0 +1,21 @@
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	if root.Left == nil {
+		return 1 + minDepth(root.Right)
+	}
+
+	if root.Right == nil {
+		return 1 + minDepth(root.Left)
+	}
+
+	return 1 + min(minDepth(root.Left), minDepth(root.Right))
+}
