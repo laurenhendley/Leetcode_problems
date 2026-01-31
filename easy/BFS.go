12e0b5debb4762c1648137d6b1286facfcb33701
laurenhendley <lauren.hendley4@gmail.com>
2026-01-31 16:39:28 -0500

    Solved BFS problem

---
+++ easy/BFS.go
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func preorderTraversal(root *TreeNode) []int {
+    res := []int{}
+    var dfs func(*TreeNode)
+
+    dfs = func(node *TreeNode){
+        if node == nil {
+            return
+        }
+        res = append(res, node.Val)
+        dfs(node.Left)
+        dfs(node.Right)
+    }
+    
+    dfs(root)
+    return res
+}
+
+
