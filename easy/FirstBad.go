8c4c070de181e85ab401ca85799aa3dcd415a39c
laurenhendley <lauren.hendley4@gmail.com>
2026-03-01 11:41:02 -0500

    Solved

---
+++ easy/FirstBad.go
@@ -0,0 +1,25 @@
+/** 
+ * Forward declaration of isBadVersion API.
+ * @param   version   your guess about first bad version
+ * @return 	 	      true if current version is bad 
+ *			          false if current version is good
+ * func isBadVersion(version int) bool;
+ */
+
+func firstBadVersion(n int) int {
+    left := 1
+    right := n
+    badV := n
+
+    for left <= right {
+        mid := left + ((right - left) / 2)
+        if midStat := isBadVersion(mid); midStat{
+            badV = mid
+            right = mid - 1
+        } else {
+            left = mid + 1
+        }
+    }
+
+    return badV
+}
