2a6f0de26d3ceb121ab51075f1cd4503c487b3a6
laurenhendley <lauren.hendley4@gmail.com>
2026-03-03 12:50:35 -0500

    Solved

---
+++ easy/moveZeroes.go
@@ -0,0 +1,9 @@
+func moveZeroes(nums []int)  {
+   left := 0
+   for right := range(nums){
+    if nums[right] != 0 {
+        nums[left], nums[right] = nums[right], nums[left]
+        left++
+    }
+   }
+}
