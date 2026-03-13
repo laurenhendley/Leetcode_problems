190f0ba1f55f3aa3095a71f783f75554d6c3dbbc
laurenhendley <lauren.hendley4@gmail.com>
2026-03-13 08:07:28 -0400

    solved

---
+++ easy/guess.go
@@ -0,0 +1,25 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * func guess(num int) int;
+ */
+
+func guessNumber(n int) int {
+    l := 1
+    h := n
+
+    for l <= h {
+        mid := l + (h-l) / 2
+        if guess(mid) == 0 {
+            return mid
+        } else if guess(mid) == -1 {
+            h = mid - 1
+        } else {
+            l = mid + 1
+        }
+    }
+    return -1
+}
